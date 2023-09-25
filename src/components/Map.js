b71ad2a960220a7222bbadf740870084f138acf4
AxelLWitt <axellwitt16@gmail.com>
2023-09-25 13:38:13 -0400

    map styling

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,45 +2,43 @@ import React, { useState } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const MapContainer = () => {
-  let API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-  const mapStyles = {        
+  let API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
+  const mapContainerStyles = {
     height: "100vh",
-    width: "100%"};
+    width: "100%"
+  };
   
   const defaultCenter = {
     lat: 40.783660, lng: -73.965019
   };
 
-  let center = defaultCenter
-  // Step 1: Maintain a state for the markers
   const [markers, setMarkers] = useState([]);
+  const [center, setCenter] = useState(defaultCenter);
 
   const handleMapClick = (event) => {
-    // Step 2: On map click, retrieve the clicked location
     const newMarker = {
       lat: event.latLng.lat(),
       lng: event.latLng.lng()
     };
-    center = newMarker
-
-    console.log(center)
-    // Step 3: Add the new marker to the markers array state
+    setCenter(newMarker);
     setMarkers(currentMarkers => [...currentMarkers, newMarker]);
   };
 
   return (
-     <LoadScript googleMapsApiKey={API_KEY}>
-        <GoogleMap
-          mapContainerStyle={mapStyles}
-          zoom={12}
-          center={center}
-          onClick={handleMapClick}  // Attach the click handler
-        >
-          {markers.map((marker, index) => (
-            <Marker key={index} position={marker} />
-          ))}
-        </GoogleMap>
-     </LoadScript>
+    <LoadScript googleMapsApiKey={API_KEY}>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyles}
+        zoom={12}
+        center={center}
+        onClick={handleMapClick}
+        options={{ mapId : "af6bc521083dc9cf" }}
+      >
+        {markers.map((marker, index) => (
+          <Marker key={index} position={marker} />
+        ))}
+      </GoogleMap>
+    </LoadScript>
   );
 }
 
